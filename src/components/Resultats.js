e1a9624803248e0480ffacec5f145ac31e58ceff
minger3u <nathan.minger9@etu.univ-lorraine.fr>
2021-05-19 11:33:53 +0200

    Utilisation des donnes de l'utilisateur

--- src/components/Resultats.js
+++ src/components/Resultats.js
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import People from '../utils/People';
 
     
     //Visuel du graph
@@ -18,7 +17,7 @@ import People from '../utils/People';
     };
 
     //Donnees du graph
-    const data = {
+    const dataex = {
       labels: ['1', '2', '3', '4', '5', '6'],
       datasets: [
         {
@@ -47,16 +46,16 @@ class Resultats extends React.Component{
 
     //verifie simplement si on a reussi a recuperer les infos de la personne
     showPerson=()=>{
-        console.log(this.state.person);
+        console.log(this.props.data.oxygen_rates);
     }
 
-    //Dans le render, on voit qu'on peut afficher un taux d'oxygenation avec this.state.person.oxygen_rates[0].percent
+    //Dans le render, on voit qu'on peut afficher un taux d'oxygenation avec this.props.data.oxygen_rates[0].percent
     render(){
     return(
         <div>
             <button onClick={this.showPerson}>click</button>
-            <div>{this.state.person !==null ? this.state.person.oxygen_rates[0].percent : null}</div>
-            <Line data={data} options={options} />
+            <div>{this.props.data !==null ? this.props.data.oxygen_rates[0].percent : null}</div>
+            <Line data={dataex} options={options} />
         </div>
     );
     }
--- src/components/VosDonnees.js
+++ src/components/VosDonnees.js
@@ -1,30 +1,60 @@
-import React, {useState} from 'react';
+import React from 'react';
 import Questionnaire from './Questionnaire';
 import Resultat from './Resultats';
 
-
-function VosDonnees(props){
-    
-
-    const[openQ, setOpenQ] = useState(false);
-
-    const clickQuestionnaire = () =>{
-        setOpenQ(!openQ);
+class VosDonnees extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            openQ:false,
+            data:null,
+            age:null,
+        }
     }
 
+    componentDidMount= async ()=>{
+        const datajs = require('../utils/patients.json');
+        this.setState({data: datajs[1]});
+        //Obtention de l'age de l'utilisateur avec sa date de naissance
+        const birthDate = datajs[1].date_of_birth;
+        const dob = new Date(birthDate);
+        const monthDiff = Date.now() - dob.getTime();
+        const age_dt = new Date(monthDiff);
+        const year = age_dt.getUTCFullYear();
+        const age = Math.abs(year - 1970);
+        this.setState({age: age});
+    }
+
+    clickQuestionnaire = () =>{
+        this.setState({openQ: !this.state.openQ});
+    }
+
+    showDetails = () => {
+        return (
+            <div>
+                <div>Mail: {this.state.data.mail}</div>
+                <div>Besoin d'une consultation: {this.state.data.need_consultation ? "Oui" : "Non"}</div>
+                <div>Localisation de la tumeur: {this.state.data.localisation_tumor}</div>
+                <div>Âge: {this.state.age}</div>
+                <div>Sexe: {this.state.data.sex}</div>
+            </div>
+        )
+    }
+
+    render(){
     return(
         <div>
             <h1>Informations</h1>
-            <p>Âge: 64</p>
-            <p>Sexe: F</p>
+            <div>{this.state.data !== null ? this.showDetails() : null}</div>
             <p>Date de l'opération: 22/03/2021</p>
             <h1>Suivi</h1>
             <p>Afin d'obtenir toutes les informations nécessaires merci de remplir chaque semaine le questionnaire suivant</p>
-            <button onClick={clickQuestionnaire} userid={props.userID}>{openQ ? 'Annuler' : 'Remplir le questionnaire'}</button>
-            {openQ ? <Questionnaire/> : ''}
-            <Resultat/>
+            <button onClick={this.clickQuestionnaire} userid={this.props.userID}>{this.state.openQ ? 'Annuler' : 'Remplir le questionnaire'}</button>
+            {this.state.openQ ? <Questionnaire/> : ''}
+            <Resultat data={this.state.data}/>
         </div>
     )
+}
 }
 
 export default VosDonnees;
